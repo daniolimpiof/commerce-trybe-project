9bfda38a5dbc1263abdb75cc7e4ba9a724addc74
daniolimpiof <danielly.fundcheller@gmail.com>
2022-09-11 15:01:06 -0300

    feat: :white_check_mark: desafio 18
    Inclua bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo

---
+++ challenges/desafio18.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+    { $or: [ 
+        { nome: { $eq: "Big Mac" } },
+        { nome: { $eq: "Quarteirão com Queijo" } }],
+    },
+    {
+        $addToSet: { ingredientes: "bacon" },
+    },
+);
+ 
+db.produtos.find({}, 
+        { _id: 0, nome: 1, ingredientes: 1 });
