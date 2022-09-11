5d1a6d5c31838ebdd9feb40314f39930a66ed5a3
daniolimpiof <danielly.fundcheller@gmail.com>
2022-09-11 15:14:55 -0300

    feat: :white_check_mark: requisito 22
    Adicione a quantidade de vendas dos sanduíches por dia da semana

---
+++ challenges/desafio22.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany({},
+    {
+        $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $set: { "vendasPorDia.3": 60 } },
+);
+
+db.produtos.updateMany(
+    { tags: "bovino" },
+    { $set: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find({},
+    { _id: 0, nome: 1, vendasPorDia: 1 });
