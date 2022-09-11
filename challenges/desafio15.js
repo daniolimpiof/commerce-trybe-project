538b8038eb71b796788e82eee2abc9900800e76a
daniolimpiof <danielly.fundcheller@gmail.com>
2022-09-11 15:50:39 -0300

    fix: :bug: acertando o numberint e o set correto para alterar a avaliação das tags correspondentes

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,7 +1,10 @@
-db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
-
-db.produtos.updateMany({ tags: "bovino" }, { $inc: { avaliacao: 5 } });
-
-db.produtos.updateMany({ tags: "aves" }, { $inc: { avaliacao: 3 } });
-
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
+db.produtos.updateMany(
+  { tags: { $in: [/bovino/i] } },
+  { $set: { avaliacao: NumberInt(5) } },
+);
+db.produtos.updateMany(
+  { tags: { $in: [/ave/i] } },
+  { $set: { avaliacao: NumberInt(3) } },
+);
 db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
