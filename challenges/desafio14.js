6b6cab2469bdd94c9a06ab2838d2af275f4bbba6
daniolimpiof <danielly.fundcheller@gmail.com>
2022-09-11 14:30:56 -0300

    feat: :white_check_mark: requisito 14
    Crie uma query que retorne todos os lanches que possuem picles em seus ingredientes e mostre apenas os 3 primeiros itens contidos no array valoresNutricionais

---
+++ challenges/desafio14.js
@@ -0,0 +1,11 @@
+db.produtos.find(
+    { ingredientes: 
+        { $elemMatch: { $eq: "picles" },
+     } },
+    {
+    _id: 0, 
+    nome: 1, 
+    ingredientes: 1,
+    valoresNutricionais: { $slice: 3 }, 
+},
+);
